2127c241d36366c1c9390778cafb88769d571198
yogi <moh.yogi10065@gmail.com>
2022-04-07 15:05:26 +0700

    add Models rfid_info

---
+++ models/RfidData.js
@@ -0,0 +1,40 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const rfid_data = sequelize.define('rfid_data', {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        reader_id: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        port: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        ip_server: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        api_server:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        createdAt:{
+            field: 'created_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt:{
+            field: 'updated_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    },{
+        tableName: 'rfid_data',
+    });
+
+    return rfid_data;
+}
