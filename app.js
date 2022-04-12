bc44fb1a8e943c17c01fe85a04151d1563abbb34
yogi <moh.yogi10065@gmail.com>
2022-04-12 11:39:34 +0700

    add css

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('/js', express.static(path.join(__dirname, 'public/javascripts')));
 
 app.use('/', viewRouter);
 app.use('/users', usersRouter);
