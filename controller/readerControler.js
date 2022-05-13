402a0b227f4b43571965f7d4a8b2279e09100d10
yogi <moh.yogi10065@gmail.com>
2022-05-13 15:07:04 +0700

    add api update mode alarm, udp, api

--- controller/readerControler.js
+++ controller/readerControler.js
@@ -1,4 +1,3 @@
-const getMac = require('getmac');
 const {rfid_data} = require('../models');
 
 
@@ -11,18 +10,52 @@ exports.showReader = async (req, res) =>{
     })
 }
 
-exports.updateReader = async (req, res) =>{
-    const {reader, type, id_account} = req.body;
-
+exports.UDP = async (req, res) =>{
+    const {reader, port, ip_server} = req.body;
+    //const id = rfid_data.findByPk(1);
     const result = await rfid_data.update({
         reader_id: reader,
-        type_reader: type,
-        id_account: id_account
-    });
+        mode: 'udp',
+        port: port,
+        ip_server: ip_server
+        }, {where : {id: 1}}
+    );
+
+    data = await rfid_data.findAll();
     return res.json({
         status: 'success',
-        data: result
-        
+        data: data
     })
 }
 
+
+exports.API = async (req, res) =>{
+    const {api_server, api_key} = req.body;
+    const result = await rfid_data.update({
+        mode: 'api',
+        api_server: api_server,
+        }, {where : {id: 1}}
+    );
+
+    data = await rfid_data.findAll();
+    return res.json({
+        status: 'success',
+        data: data
+    });
+}
+
+
+exports.ALARM = async (req, res) =>{
+    const {api_server, api_key} = req.body;
+    const result = await rfid_data.update({
+        mode: 'alarm',
+        api_server: api_server,
+        }, {where : {id: 1}}
+    );
+
+    data = await rfid_data.findAll();
+    return res.json({
+        status: 'success',
+        data: data
+    });
+}
--- routes/reader.js
+++ routes/reader.js
@@ -4,8 +4,12 @@ const readerController = require('../controller/readerControler');
 
 
 router.get('/', readerController.showReader);
-router.get('/', (req, res) =>{
-  res.render('views/index');
-})
+// router.get('/', (req, res) =>{
+//   res.render('views/index');
+// })
+
+router.put('/udp', readerController.UDP);
+router.put('/api', readerController.API);
+router.put('/alarm', readerController.ALARM);
 
 module.exports = router;
